f17d540492758f7fede1100f639c2fe2b376db2b
yousra <yousraniati0@gmail.com>
2024-01-06 12:25:29 +0100

    added start button & menu bar

--- src/algo.c
+++ src/algo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL.h>
-
+#include <SDL_ttf.h>
 
 // Structure pour représenter un nœud de la liste doublement chaînée
 
@@ -49,7 +49,7 @@ void highlightComparison(struct Node* node1, struct Node* node2, SDL_Renderer* r
     SDL_Color currentColor;
     SDL_GetRenderDrawColor(renderer, &currentColor.r, &currentColor.g, &currentColor.b, &currentColor.a);
     // Couleur rouge pour la mise en évidence
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
 
     // Dessiner les rectangles en rouge
     SDL_Rect rect1 = {node1->xpos, 400 - node1->data * 15, 35, node1->data * 15};
@@ -69,23 +69,51 @@ void highlightComparison(struct Node* node1, struct Node* node2, SDL_Renderer* r
     SDL_RenderPresent(renderer);
 }
 
+ // Function to render text
+void renderText(SDL_Renderer* renderer, int x, int y, TTF_Font* font, int value) {
+    SDL_Color textColor = {255, 255, 255, 255};
+    char numberText[10];  // Assuming a maximum of 10 characters for the number
+    snprintf(numberText, sizeof(numberText), "%d", value);
+
+    SDL_Surface* textSurface = TTF_RenderText_Solid(font, numberText, textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    SDL_Rect textRect = {x, y, 30, 20};  // Adjust the position and size as needed
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
+}
+
+void rendertext(SDL_Renderer* renderer, int x, int y, TTF_Font* font, const char* text, SDL_Color textColor) {
+
+    SDL_Surface* textSurface = TTF_RenderText_Blended(font, text, textColor);
+    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    SDL_Rect textRect = {x, y, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+    SDL_FreeSurface(textSurface);
+    SDL_DestroyTexture(textTexture);
+}
 
 // Fonction pour trier la liste doublement chaînée par sélection
 
-void selectionSort(struct Node* head, SDL_Renderer* renderer) {
+void selectionSort(struct Node* head, SDL_Renderer* renderer,TTF_Font* font) {
 
     struct Node *i, *j, *minNode;
     // Afficher la liste avant le tri
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     struct Node* current = head;
     int xPos = 200;
 
     while (current != NULL) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_Rect rect = {xPos, 400 - current->data * 15, 35, current->data * 15};
         current->xpos = xPos;
         SDL_RenderFillRect(renderer, &rect); // Couleur dans les rectangles
+        renderText(renderer, xPos, 410, font, current->data);
         xPos += 60;
         current = current->next;
     }
@@ -106,7 +134,7 @@ void selectionSort(struct Node* head, SDL_Renderer* renderer) {
     swapData(i, minNode);
 
     // Remplir le fond en blanc
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     // Dessiner les rectangles
@@ -114,9 +142,10 @@ void selectionSort(struct Node* head, SDL_Renderer* renderer) {
     xPos = 200;
 
     while (current != NULL) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_Rect rect = {xPos, 400 - current->data * 15, 35, current->data * 15};
         SDL_RenderFillRect(renderer, &rect);
+        renderText(renderer, xPos, 410, font, current->data);
         xPos += 60;
         current = current->next;
     }
@@ -125,13 +154,20 @@ void selectionSort(struct Node* head, SDL_Renderer* renderer) {
     SDL_Delay(1000); // Attendre 1000 millisecondes entre chaque étape
 }
 }
-
+int isMouseOverButton(int mouseX, int mouseY, SDL_Rect buttonRect) {
+    return mouseX >= buttonRect.x && mouseX <= buttonRect.x + buttonRect.w &&
+           mouseY >= buttonRect.y && mouseY <= buttonRect.y + buttonRect.h;
+}
 int main(int argc, char* argv[]) {
 
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+
+    TTF_Init();
+    TTF_Font* font = TTF_OpenFont("bin\\arial.ttf", 24);
+    SDL_Event e;
     // Création d'une liste doublement chaînée non triée
 
     struct Node* head = NULL;
@@ -149,32 +185,52 @@ int main(int argc, char* argv[]) {
     struct Node* current = head;
 
     while (current != NULL) {
-
         printf("%d ", current->data);
-
         current = current->next;
-
     }
 
     printf("\n");
 
-    // Tri par sélection avec visualisation
-    selectionSort(head, renderer);
-    printf("Liste doublement chaînée triée\n");
 
-    // Afficher la liste après le tri
-    current = head;
-    while (current != NULL) {
-        printf("%d ", current->data);
+    // Create "Start" button
+    SDL_Rect buttonRect = {350, 500, 100, 50};
+    SDL_Color buttonColor = {100, 100, 100, 255};
 
-        current = current->next;
+    SDL_SetRenderDrawColor(renderer, buttonColor.r, buttonColor.g, buttonColor.b, buttonColor.a);
+    SDL_RenderFillRect(renderer, &buttonRect);
 
+    SDL_Color textColor = {255, 0, 255, 255};
+    rendertext(renderer, buttonRect.x + 15, buttonRect.y + 15, font, "Start", textColor);
+
+    // Render "SELECTION CODE" in the middle
+    SDL_Color selectionTextColor = {150, 100, 150, 255};
+    rendertext(renderer, 280, 220, font, "SELECTION SORT", selectionTextColor);
+
+    SDL_RenderPresent(renderer);
+
+    // Event loop
+    int sortingStarted = 0;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
+            } else if (e.type == SDL_MOUSEBUTTONDOWN) {
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+
+                if (isMouseOverButton(mouseX, mouseY, buttonRect) && !sortingStarted) {
+                    selectionSort(head, renderer, font);
+                    sortingStarted = 1;
+                }
+            }
+        }
     }
+
     printf("\n");
     // Attendre que l'utilisateur ferme la fenêtre
     SDL_Delay(2000);
-    int quit = 0;
-    SDL_Event e;
+    
     while (!quit) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
@@ -182,6 +238,9 @@ int main(int argc, char* argv[]) {
             }
         }
     }
+
+    TTF_CloseFont(font);
+    TTF_Quit();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
